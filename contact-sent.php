f71779e7e05dd3d01ef3ff4ae87e83d44c7943ee
Emils <emils@gmail.com>
2025-04-22 14:47:21 +0300

    first commit and last

---
+++ contact-sent.php
@@ -0,0 +1,11 @@
+
+
+<?php
+
+echo "<h1>Savienojums veiksmīgs! </h1>";
+
+echo $_POST['name'];
+echo $_POST['email'];
+echo $_POST['message'];
+?>
+
---
+++ sql-store.php
@@ -0,0 +1,63 @@
+<!-- 3.uzdevums (Dati no MySQL datubāzes)
+Izveidojiet jaunu MySQL lietotāju, kam ir pilna piekļuve 'sql_store' datubāzei.
+Pieslēdzieties datubāzei izmantojot šo lietotāju jaunā failā 'sql-store.php'.
+Atgrieziet datus no 'customers' tabulas kā HTML sarakstu. -->
+
+<?php
+$servername = "localhost";  
+$username = "user111"; 
+$password = "password";  
+$dbname = "sql_store";  
+
+
+try {
+    
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    
+   
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    
+    $stmt = $conn->prepare("SELECT * FROM customers");
+    $stmt->execute();
+    
+    
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+    echo "<ul>";
+    foreach ($result as $row) {
+        echo "<li>";
+        echo "ID: " . $row["customer_id"] . "</div>";
+        echo " " . $row["first_name"] . " " . $row["last_name"] . "</div>";
+        echo " ";
+        
+        echo "Tālrunis: " . $row["phone"] . "<br>";
+        if (isset($row["email"])) {
+            echo "E-pasts: " . $row["email"] . "<br>";
+        }
+        echo "</div>";
+        echo "</li>";
+    }
+    echo "</ul>";
+} 
+
+
+
+
+
+
+
+
+
+
+
+ catch(PDOException $e) {
+    echo "Savienojuma kļūda: " . $e->getMessage();
+}
+
+
+    $conn = null;
+
+?>
